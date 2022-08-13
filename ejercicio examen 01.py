a2fdf9ca211afa02dcb7351a9bc6aa0f448ef541
Yessi0908 <106893050+Yessi0908@users.noreply.github.com>
2022-08-13 10:44:54 -0600

    Add files via upload

---
+++ ejercicio examen 01.py
@@ -0,0 +1,9 @@
+
+import math
+
+LadoA = float(input("Escriba una cantidad: "))
+LadoB = float(input("Escriba una cantidad: "))
+
+hipo = math.sqrt((LadoA**2) + (LadoB**2))
+
+print("La hipotenusa es: ", hipo)
